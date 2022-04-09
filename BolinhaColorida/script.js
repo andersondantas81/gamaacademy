dd9dc02e266e635da62363cdf4fe4224464fb93b
Anderson Dantas <andersondantas81@gmail.com>
2022-04-09 09:46:48 -0300

    projeto bolinha-colorida

---
+++ BolinhaColorida/script.js
@@ -0,0 +1,16 @@
+function corBackground(cor){
+
+  if(cor == 'yellow'){
+    document.body.style.backgroundColor = 'yellow';
+  }
+  if(cor == 'green'){
+    document.body.style.backgroundColor = 'green';
+  }
+  if(cor == 'red'){
+    document.body.style.backgroundColor = 'red';
+  }
+  if(cor == 'blue'){
+    document.body.style.backgroundColor = 'blue';
+  }
+  
+}
